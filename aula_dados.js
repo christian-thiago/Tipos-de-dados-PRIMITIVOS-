d36d7754e135ea7f66efdbd6e936c50a204ae8a4
christian <christianthiagooficial@gmail.com>
2025-01-30 21:59:05 -0300

    Adicionando aula_dados.js

---
+++ aula_dados.js
@@ -0,0 +1,19 @@
+// Tipos de dados PRIMITIVOS 
+// Number, String, Boolean, UNdefined, Null
+
+ let numero = 10;
+ let valor = 20.40; // float.
+
+ //string 
+ let nome= "fulano";
+
+ //boolean // Verdadeiro (true) ou falso (false);
+ let freteGratis = true;
+
+ // Undefined: Representa a ausência do valor. Geralmente, é o valor padrão para variáveis recem-declaradas.*/
+ let desconto;
+
+ // Null - Representa a ausência intencional de qualquer valor ou objeto.
+ let total= null;
+  
+ console.log (typeof total)
