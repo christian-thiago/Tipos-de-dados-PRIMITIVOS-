058bcb99d0db3ab2f764eb732a8bc02cbdf592b0
christian <christianthiagooficial@gmail.com>
2025-01-31 04:46:48 -0300

    Adicionando aula_const.js

---
+++ aula_const.js
@@ -0,0 +1,13 @@
+// tipo constante 
+
+let valor = 100
+valor = 300
+valor = "Programador"
+
+// ---------------------
+// Constante - variável cujo valor não pode ser reatribuido após a sua inicíalízação. ou seja, uma vez que voçê atribuiu um valor a uma variável usando const, não pode atribuir um valor diferente a essa variavel. //
+
+
+const idade = 28;
+
+console.log(idade)
