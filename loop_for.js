93a82ef538351adec9661965f49c2905e436fe4c
christian <christianthiagooficial@gmail.com>
2025-02-08 21:21:20 -0300

    Adicionando meu exercício

--- loop_for.js
+++ loop_for.js
@@ -1,5 +1,17 @@
-// FOR > é usado quando você sabe quantas vezes deseja repetir um bloco de código
+// FOR > So é usado quando você sabe quantas vezes deseja repetir um bloco de código
 
-for (let i = 0; i < 11; i++) {
+/*
+ for (let i = 0; i < 11; i++) {
   console.log("repetição número " + i)
 }
+*/
+
+const numero = 2;
+let valor = 10;
+
+for (let i = 0; i <= numero; i++) {
+  console.log("numero sendo passado" + i)
+  valor = valor + 5;
+}
+
+console.log(valor);
